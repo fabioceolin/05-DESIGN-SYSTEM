f478f7025e054f33f6a4fc16e75e0a7d6bcca45f
Fábio Ceolin <fabioceolin98@gmail.com>
2023-01-30 14:22:52 -0300

    Avatar and Button components

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar() {
+  return (
+    <AvatarContainer>
+      <AvatarImage />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
